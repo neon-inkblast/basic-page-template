684c429d104deca8a491b10138b57ce1f18dcb1f
Ben <ben.atkinson.official@gmail.com>
2021-09-12 12:26:28 +1000

    Theme picker implemented to test out different colours

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,8 @@
     const successBtn = document.querySelector(".btn.success");
     const warningBtn = document.querySelector(".btn.warning");
     const errorBtn = document.querySelector(".btn.error");
+
+    const colorPickers = document.querySelectorAll(".color-input");
     
     darkBtn.addEventListener("click", () => {
         document.body.classList.remove("light");
@@ -27,10 +29,22 @@
         lightBtn.classList.add("active");
     });
 
+    // contextual labels
     const notifyFn = (type) => () => document.querySelector(".notification-box").setAttribute("class", `notification-box ${type}`);
+
     infoBtn.addEventListener("click", notifyFn("info"));
     successBtn.addEventListener("click", notifyFn("success"));
     warningBtn.addEventListener("click", notifyFn("warning"));
     errorBtn.addEventListener("click", notifyFn("error"));
 
+    // color pickers
+
+    Array.from(colorPickers).forEach((picker) => {
+        picker.addEventListener("change" ,() => {
+            document.documentElement.style.setProperty("--c-primary", colorPickers[0].value);
+            document.documentElement.style.setProperty("--c-primary-accent", colorPickers[1].value);
+            document.documentElement.style.setProperty("--c-secondary", colorPickers[2].value);
+            document.documentElement.style.setProperty("--c-secondary-accent", colorPickers[3].value);
+        });
+    })
 })()
